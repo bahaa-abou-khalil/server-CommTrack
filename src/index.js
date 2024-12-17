4ad60f7cd173e25bc204b8dfb185345b112fa4be
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-18 00:52:34 +0200

    creating route for slack apis

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import connectDatabase from "./db/connection.js";
 import userRoutes from "./routes/users.routes.js";
+import slackRoutes from "./routes/slack.routes.js";
 
 
 const app =express();
 app.use(express.json());
 
 app.use("/users", userRoutes);
-
+app.use("/slack",slackRoutes)
 app.listen(8080,async()=>{
     console.log("Server running on port 8080");
     connectDatabase();
---
+++ src/routes/slack.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import {
+    getMessages
+} from "../controllers/slack/messages.controller.js"
+
+const router = new Router();
+
+router.get("/", getMessages);
+
+export default router;
