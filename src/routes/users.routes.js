d9f706ea3117006618375b424275fcb967fd8488
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-17 18:45:02 +0200

    adding route to createUser

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import {
+    createUser,
     getUsers
 } from "../controllers/users.controller.js"
 
 const router = new Router();
 
 router.get("/:id?", getUsers);
+router.post("/",createUser);
 
 export default router;
