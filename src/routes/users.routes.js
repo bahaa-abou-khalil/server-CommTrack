8b1229e2910d29ea9afd13957e4f3541ef6d8398
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-17 13:39:04 +0200

    creating a router for users

---
+++ src/routes/users.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import {
+    getUsers
+} from "../controllers/users.controller.js"
+
+const router = new Router();
+
+router.get("/:?id",getUsers);
+
+export default router;
