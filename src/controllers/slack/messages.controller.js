dedd7cce862350a799eb29ad737c157dc9044c85
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-19 00:50:10 +0200

    editing getMessages function from slack/message controller

--- src/controllers/slack/messages.controller.js
+++ src/controllers/slack/messages.controller.js
@@ -8,18 +8,22 @@ const slackClient = new WebClient(token);
 export const getMessages = async (req, res) => {
   try {
     const { channels } = await slackClient.conversations.list();
+    const allMessages = [];
 
     for (const channel of channels) {
       console.log(`Fetching messages from channel: ${channel.name}`);
       
-      const messages = await slackClient.conversations.history({
+      const { messages } = await slackClient.conversations.history({
         channel: channel.id,
       });
-      
-      console.log(messages.messages);
-    }
 
-    res.status(200).json({ message: "Messages fetched successfully" });
+      allMessages.push({
+        channelName: channel.name,
+        messages: messages,
+      });
+    }
+    
+    res.status(200).json({ message: "Messages fetched successfully", data: allMessages });
   } catch (error) {
     console.error(`Error fetching messages: ${error.message}`);
     res.status(500).json({ message: "Error fetching messages from Slack" });
--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import slackRoutes from "./routes/slack.routes.js";
 const app =express();
 app.use(express.json());
 
-app.use("/users", userRoutes);
-app.use("/slack",slackRoutes)
 app.listen(8080,async()=>{
     console.log("Server running on port 8080");
-    connectDatabase();
+    await connectDatabase();
 })
+
+app.use("/users", userRoutes);
+app.use("/slack",slackRoutes)
