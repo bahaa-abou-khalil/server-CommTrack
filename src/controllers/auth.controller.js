3d1193c7758aea2519a50d9f4132d472a34468b0
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-19 17:48:27 +0200

    adding basic password validation for signup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import emailValidator from 'email-validator';
-
+import passwordValidator from "password-validator";
 
 export const signUp = async (req, res) => {
     const { fName, lName, email, password } = req.body;
@@ -15,11 +15,18 @@ export const signUp = async (req, res) => {
       }
 
       const validEmail = emailValidator.validate(email);
-      if(!validEmail){
-        console.log(validEmail)
+      const {errorMessage, validPassword} = validatePassword(password);
+      
+      if(!validEmail ){
         return res.status(500).send({
             message: "Please provide a valid email.",
           });   
+      }
+      else if(!validPassword){
+        return res.status(500).send({
+            message: "Please provide a valid password.",
+            reason: String(errorMessage),
+          }); 
       }
 
       const hashed = await bcrypt.hash(password, 10);
@@ -40,3 +47,13 @@ export const signUp = async (req, res) => {
       });
     }
   };
+
+const validatePassword = (password)=>{
+    let schema = new passwordValidator();
+    schema
+    .is().min(6)
+    const error = schema.validate(password, { details: true });
+    const errorMessage = String(error.map(error => error.message));
+    const validPassword = schema.validate(password)
+    return {errorMessage, validPassword}
+}
