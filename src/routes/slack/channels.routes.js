964a508076856015f2fdb5c6630b94c0eb170527
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-19 04:07:59 +0200

    creating route to redirect to channel

--- src/routes/slack/channels.routes.js
+++ src/routes/slack/channels.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import {
     createChannel,
-    getChannels
+    getChannels,
+    redirectToChannel
 } from "../../controllers/slack/channels.controller.js"
 
 const router = new Router();
 
 router.post("/", createChannel);
 router.get("/", getChannels);
+router.get("/redirect/:channelName", redirectToChannel);
 
 export default router;
