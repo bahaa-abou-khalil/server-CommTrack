a61847cb71cd44b55df274edc539f082f874b4a0
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-21 23:11:20 +0200

    linking passport in googleauth to database (still in testing)

--- src/middleware/googleAuth.js
+++ src/middleware/googleAuth.js
@@ -1,39 +1,46 @@
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { User } from '../models/user.model.js';
-import passport from "passport"
+import passport from "passport";
 
-    passport.use(
-        new GoogleStrategy(
+passport.use(
+    new GoogleStrategy(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: "http://localhost:8080/google/callback",
-            passReqToCallback: true // check
         },
-        async (request, accessToken, refreshToken, profile, done) => {
-
-            // User.findOrCreate({ googleID: profile.id }, (error, user)=>{
-            
-                return done(null,profile);
-            // }
-       
-
+        async (accessToken, refreshToken, profile, done) => {
+            console.log("Access Token:", accessToken);
+            console.log("Profile:", profile);
+            try {
+                let user = await User.findOne({ googleID: profile.id });
+                if (!user) {
+                    user = await User.create({
+                        googleID: profile.id,
+                        email: profile.emails[0]?.value,
+                        name: profile.displayName
+                    });
+                }
+                return done(null, user);
+            } catch (error) {
+                console.error("Error in Google Strategy:", error);
+                return done(error, null);
+            }
         }
-        )
-    );
-    
-    passport.serializeUser((user, done) => {
-    done(null, user);
-    });
+    )
+);
 
-    // could be like serialization
-    passport.deserializeUser((user, done)=> {
-    // try {
-        // const user = await User.findById(id);
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
         done(null, user);
-    // } catch (error) {
+    } catch (error) {
+        done(error, null);
+    }
+});
 
-
-})
-
-export const authenticateGoogle = passport.authenticate('google', { scope: ['email'] });
+export const authenticateGoogle = passport.authenticate('google', { scope: ['email','profile'] });
