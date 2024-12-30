c37d5debccda57b7067c330417aefe97244b1e7c
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-30 15:33:22 +0200

    adding route for sign in callback

--- src/routes/slack/signIn.routes.js
+++ src/routes/slack/signIn.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import {
-    signInSlack
+    signInSlack,
+    signInCallback
 } from "../../controllers/slack/signIn.controller.js"
 
 const router = new Router();
 
 router.get("/", signInSlack);
+router.get("/welcome", signInCallback);
 
 export default router;
