8d720d7046dce2fddb5c6ba87398bd8ab756910a
bahaaCs <bahaaaboukhalil@gmail.com>
2025-01-01 01:29:47 +0200

    <folderStructure> editing connection file in db

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,11 +2,17 @@ import { connect } from "mongoose";
 
 const connectToDatabase = async () => {
   try {
-    await connect("mongodb://127.0.0.1:27017/comm-track");
+    const dbHost = process.env.DB_HOST;
+    const dbPort = process.env.DB_PORT;
+    const dbName = process.env.DB_NAME;
+
+    const url = `${dbHost}:${dbPort}/${dbName}`;
+
+    await connect(url);
 
     console.log("Connected to database");
   } catch (error) {
-    console.log(`error connecting to database: ${error}`);
+    console.log(error.message);
   }
 };
 
