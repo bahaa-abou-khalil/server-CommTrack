2caf661d5006f66397222ea40c0b041149f00f1f
bahaaCs <bahaaaboukhalil@gmail.com>
2025-01-07 02:09:52 +0200

    adding route to getActiveUser controller

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -3,12 +3,15 @@ import { AppRouter } from "../../config/AppRouter.js";
 import {
     createUser,
     getUsers,
-    getSlackUsersByIds
+    getSlackUsersByIds,
+    getActiveUser
     } from "./users.controller.js"
 import { adminMiddleware } from "../../middlewares/admin.middleware.js";
 
 const usersRouter = new Router();
 
+
+usersRouter.get("/active", getActiveUser);
 usersRouter.get("/:id?", getUsers);
 usersRouter.post("/",createUser);
 usersRouter.post("/slackUsers",getSlackUsersByIds);
