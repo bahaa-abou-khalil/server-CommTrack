36f3a262652290cd28f59424427aaffa2ad8ad94
bahaaCs <bahaaaboukhalil@gmail.com>
2025-01-09 00:04:47 +0200

    moving getMessages function to messages.service file

--- src/modules/messages/messages.controller.js
+++ src/modules/messages/messages.controller.js
@@ -1,44 +1,5 @@
 import { slackClient } from "../../index.js";
 
-export const getMessages = async (req, res) => {
-  const { channelId } = req.params;
-
-  try {
-    if (!channelId) {
-      return res.status(400).json({ message: "Channel ID is required" });
-    }
-////
-// await slackClient.conversations.unarchive({ channel: channelId });
-// await slackClient.conversations.join({
-//   channel: channelId,
-// });
-///
-
-
-    const response = await slackClient.conversations.history({
-      channel: channelId,
-      limit: 100,
-    });
-
-    const filteredMessages = response.messages.filter(
-      (msg) => !msg.subtype && msg.user && msg.text && msg.ts
-    );
-
-    const formattedMessages = filteredMessages.map((msg) => ({
-      user: msg.user,
-      text: msg.text,
-      timestamp: msg.ts,
-    }));
-
-    res.status(200).json({
-      messages: formattedMessages,
-    });
-  } catch (error) {
-    console.error(`Error fetching messages: ${error.message}`);
-    res.status(500).json({ message: "Error fetching messages", error: error.message });
-  }
-};
-
 export const postMessageToChannel = async (req, res) => {
     const { channelName, message } = req.body;
   
--- src/modules/messages/messages.routes.js
+++ src/modules/messages/messages.routes.js
@@ -1,14 +1,12 @@
 import { Router } from "express";
 import { AppRouter } from "../../config/AppRouter.js";
 import {
-    getMessages,
     postMessageToChannel
     } from "./messages.controller.js"
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
 
 const messagesRouter = new Router();
 
-messagesRouter.get("/:channelId?", getMessages);
 messagesRouter.post("/", postMessageToChannel);
 
 const router = new AppRouter({
