49f3e8de444e647f76fc9a5f6720db97a60553fe
bahaaCs <bahaaaboukhalil@gmail.com>
2025-01-09 01:16:34 +0200

    creating scheduleDiscussionActions function in disc service file

--- src/modules/discussions/discussions.service.js
+++ src/modules/discussions/discussions.service.js
@@ -1,6 +1,8 @@
 import { slackClient } from "../../index.js";
 import schedule from 'node-schedule';
-import { User } from "../../db/models/user.model.js";
+import { getMessages } from "../messages/messages.service.js";
+import { analyzeMessages } from "../openAI/openAI.service.js";
+import { storeUsersAlerts } from "../alerts/alerts.service.js";
 
 export const formatDate = (timestamp) =>{
     const date = new Date(timestamp * 1000);
@@ -27,20 +29,21 @@ const archiveDiscussion = async (channelId) =>{
     }
 }
 
-export const scheduleDiscussionActions = (minutes, channelId = null) => {
+
+export const scheduleDiscussionActions = (minutes, channelId) => {
 
     let endTime = null
     if (minutes) {
         endTime = new Date();
-        endTime.setTime(endTime.getTime() + (minutes * 60 * 1000));
-        console.log(endTime)
-        
-        
+        endTime.setTime(endTime.getTime() + (minutes * 60 * 1000));             
     }
 
     if (endTime) {
         schedule.scheduleJob(endTime, async () => {
-
+            const messages = await getMessages(channelId);
+            const alertsResponse = await analyzeMessages(messages);
+            storeUsersAlerts(alertsResponse);
+            archiveDiscussion(channelId);
         });
     }
 }
