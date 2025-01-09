8094032ac71ff2539766e54663851c9651e15994
bahaaCs <bahaaaboukhalil@gmail.com>
2025-01-09 16:23:41 +0200

    creating controller to acknowledge an alert

--- src/modules/alerts/alerts.controller.js
+++ src/modules/alerts/alerts.controller.js
@@ -16,3 +16,30 @@ export const getUserAlerts = async (req,res) => {
         res.status(500).send({message: "Error occured in showing alerts."})
     }
 } 
+
+
+export const acknowledgeAlert = async (req, res) => {
+    const { alertId } = req.body;
+    const userId = req.user.id;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(500).json({ message: "Something went wrong." });
+        }
+
+        const alert = user.alerts.find(alert => alert._id.toString() === alertId);
+        if (!alert) {
+            return res.status(500).json({ message: "Something went wrong." });
+        }
+
+        alert.isAcknowledged = true;
+
+        await user.save();
+
+        res.status(200).json({ message: "Alert acknowledged successfully" });
+    } catch (error) {
+        console.error(`Error in acknowledging alert: ${error}`);
+        res.status(500).json({ error: "Something went wrong." });
+    }
+};
