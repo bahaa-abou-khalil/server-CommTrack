9e9f7a56dbdd208d776f29dc017438a9cf85ba69
bahaaCs <bahaaaboukhalil@gmail.com>
2025-01-09 03:28:46 +0200

    creating getUserAlerts controller

--- src/modules/alerts/alerts.controller.js
+++ src/modules/alerts/alerts.controller.js
@@ -1,5 +1,18 @@
+import { User } from "../../db/models/user.model.js";
 import { storeUsersAlerts } from "./alerts.service.js";
 
 export const getUserAlerts = (req,res) => {
-    return res.json({});
+    const id = req.user.id;
+
+    try{
+        const user = User.findById(id)
+
+        res.json({
+            alerts: user.alerts
+        })
+
+    }catch(error){
+        console.log(`Error getting user alerts: ${error}`)
+        res.status(500).send({message: "Error occured in showing alerts."})
+    }
 } 
