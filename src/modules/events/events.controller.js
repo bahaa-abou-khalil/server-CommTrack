44caf226d51f9e0c90c2e337d2ca490ebc29f162
bahaaCs <bahaaaboukhalil@gmail.com>
2025-01-12 05:15:26 +0200

    removing changes (was ngrok issue)

--- src/modules/events/events.controller.js
+++ src/modules/events/events.controller.js
@@ -1,27 +1,21 @@
 import { User } from "../../db/models/user.model.js";
 import { Discussion } from "../../db/models/discussion.model.js";
 import { io } from "../../index.js";
-
 export const trackStoreJoin = async (req, res) => {
     try {
         const { event } = req.body;
-        console.log("Request received with event:", event);
-        
+
         if (event.type === "member_joined_channel") {
             const channelId = event.channel;
             const slackUserID = event.user;
 
-            console.log("Channel ID:", channelId, "User ID:", slackUserID);
-
             const discussion = await Discussion.findOne({ channelId });
             if (!discussion) {
-                console.log("Discussion not found for channel:", channelId);
                 return res.status(500).json({ message: "Discussion not found." });
             }
 
-            const user = await User.findOne({ slackUserID });
+            const user = await User.findOne({ slackUserID })
             if (!user) {
-                console.log("User not found for slackUserID:", slackUserID);
                 return res.status(500).json({ message: "User not found." });
             }
 
@@ -31,10 +25,12 @@ export const trackStoreJoin = async (req, res) => {
             const discussions = await Discussion.find().populate({
                 path: "createdBy",
                 select: "profilePicture fullName",
-            });
+            })
+
+
 
             io.emit('usersUpdated', discussions);
-            console.log("Discussions updated and emitted to frontend.");
+            console.log("emitted")
 
             return res.status(200).send("Member join tracked.");
         }
