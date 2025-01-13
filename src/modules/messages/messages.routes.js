34c965f9ba9e3a9c36bf665d1ff04b0caf01af9f
bahaaCs <bahaaaboukhalil@gmail.com>
2025-01-13 10:01:27 +0200

    creating route for previous controller

--- src/modules/messages/messages.routes.js
+++ src/modules/messages/messages.routes.js
@@ -1,18 +1,20 @@
 import { Router } from "express";
 import { AppRouter } from "../../config/AppRouter.js";
 import {
-    postMessageToChannel
+    postMessageToChannel,
+    getMessages
     } from "./messages.controller.js"
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
 
 const messagesRouter = new Router();
 
+messagesRouter.get("/:channelId",getMessages)
 messagesRouter.post("/", postMessageToChannel);
 
 const router = new AppRouter({
     prefix: "/messages",
     router: messagesRouter,
-    middlewares: [],
+    middlewares: [authMiddleware],
   });
 
 export default router;
