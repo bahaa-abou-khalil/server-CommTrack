18a087e54da27b2ee4809f14a006ea862e0717ba
bahaaCs <bahaaaboukhalil@gmail.com>
2025-01-14 01:05:11 +0200

    creating route for previous controller

--- src/modules/alerts/alerts.routes.js
+++ src/modules/alerts/alerts.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { AppRouter } from "../../config/AppRouter.js";
 import { getUserAlerts 
-    , acknowledgeAlert
+    , acknowledgeAlert,
+    getAlertsStats
 } from "./alerts.controller.js";
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
 
 const alertsRouter = new Router();
 
 alertsRouter.get("/",getUserAlerts)
+alertsRouter.get("/stats",getAlertsStats)
 alertsRouter.get("/:alertId",acknowledgeAlert)
 
 const router = new AppRouter({
