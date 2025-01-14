72830062af4cbfbb968e20ec52e85695cbd850fd
bahaaCs <bahaaaboukhalil@gmail.com>
2025-01-14 03:04:17 +0200

    creating leader board routes file

--- src/modules/leaderBoard/leaderBoard.routes.js
+++ src/modules/leaderBoard/leaderBoard.routes.js
@@ -1,34 +1,18 @@
-import { Discussion } from "../../db/models/discussion.model.js";
-import { User } from "../../db/models/user.model.js";
+import { Router } from "express";
+import { AppRouter } from "../../config/AppRouter.js";
+import {
+    getLeaderBoard
+    } from "./leaderBoard.controller.js"
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
 
-export const getLeaderBoard= async (req,res) => {
-    try{
+const leaderBoardRouter = new Router();
 
-        const users = await User.find();
+leaderBoardRouter.get("/",getLeaderBoard)
 
-        const usersStats = []
+const router = new AppRouter({
+    prefix: "/leaderBoard",
+    router: leaderBoardRouter,
+    middlewares: [authMiddleware],
+  });
 
-        for (const user of users) {
-            const totalAlerts = user.alerts.length;
-
-            const discussions = await Discussion.find({ "joinedUsers.user": user.id });
-            const totalDiscussions = discussions.length;
-            usersStats.push({
-                avatar: user.profilePicture,
-                name: user.fullName,
-                email: user.email,
-                discussions: totalDiscussions,
-                alerts: totalAlerts
-            })
-
-        }
-
-        res.status(200).json(usersStats)
-    }catch(error){
-        console.log(`error in leader board : ${error}`);
-        res.status(500).json({
-            message: 'Error occured.'
-        })
-    }
-
-}
+export default router;
